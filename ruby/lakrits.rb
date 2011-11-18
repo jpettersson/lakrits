45aa004929b81e743d1e1403bf7d17c7d28f01ce
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2011-11-18 15:52:10 -0500

    Added GPL license

--- ruby/lakrits.rb
+++ ruby/lakrits.rb
@@ -1,5 +1,26 @@
 #!/usr/bin/ruby 
 
+#  lakrits.rb - Server for transporting data somewhat safely across a serial connection, using the lakrits communications protocol.
+#  The protocol supports basic addressing and error checking, but that's about it.
+#
+#  Copyright 2011 Jonathan A Pettersson  
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+#
+#  For the latest source and documentation, visit:
+#  https://github.com/jpettersson/lakrits
+
 require 'rubygems'
 require 'serialport'
     
