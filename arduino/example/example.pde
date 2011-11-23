2e9ed958040f6deef79ac7355e0335d49a5d6c68
Jonathan Pettersson <jonathan.anders.pettersson@gmail.com>
2011-11-22 22:01:26 -0500

    Switched to the MIT License

--- arduino/example/example.pde
+++ arduino/example/example.pde
@@ -1,25 +1,12 @@
 /*
-example.pde
-
-Copyright 2011 Jonathan A Pettersson  
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+example.pde - Example arduino implementation of the lakrits communication protocol.
 
 For the latest source and documentation, visit:
 https://github.com/jpettersson/lakrits
-*/
 
+Copyright 2011 Jonathan A Pettersson  
+You may use this software under the terms of the MIT License.
+*/
 
 #include <Lakrits.h>;
 
--- ruby/example.rb
+++ ruby/example.rb
@@ -1,22 +1,11 @@
-#  example.rb
-#
-#  Copyright 2011 Jonathan A Pettersson  
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+#  example.rb - Example ruby implementation of the lakrits communication protocol.
 #
 #  For the latest source and documentation, visit:
 #  https://github.com/jpettersson/lakrits
+
+#  Copyright 2011 Jonathan A Pettersson  
+#  You may use this software under the terms of the MIT License.
+
 
 require '/Users/tfh/Projects/lakrits/ruby/lakrits.rb'
 
